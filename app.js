3a4038ed5f8a0ccd5319588635cb1720d6434e08
Tracey <radcliffe.tracey@gmail.com>
2016-04-15 15:21:25 -0700

    finished problem 6

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 function sum(a, b) {
   var y = a + b;
-  // console.log('"The sum of ' + a + ' and ' + b + ' is ' + y + '."');
+  var prob1 = document.getElementById('prob1');
+  prob1.textContent = '"The sum of ' + a + ' and ' + b + ' is ' + y + '."';
   return y;
 }
 
 function multiply(a, b) {
   var z = a * b;
-  // console.log('"The product of ' + a + ' and ' + b + ' is ' + z + '."');
+  var prob2 = document.getElementById('prob2');
+  prob2.textContent = '"The product of ' + a + ' and ' + b + ' is ' + z + '."';
   return z;
 }
 
 function sumAndMultiply(d, e, f) {
   var newAry = [];
   var z = sum(sum(d, e), f);
-  console.log('"' + d + ' and ' + e + ' and ' + f + ' sum to ' + z + '."');
+  var prob3_1 = document.getElementById('prob3_1');
+  prob3_1.textContent = '"' + d + ' and ' + e + ' and ' + f + ' sum to ' + z + '."';
   newAry.push(z);
+
   var y = multiply(multiply(d, e),f);
   newAry.push(y);
-  console.log(emptyArray);
-  console.log('"The numbers ' + d + ' and ' + e + ' and ' + f + ' have a product of ' + y + '."');
+  var prob3_2 = document.getElementById('prob3_2');
+  prob3_2.textContent = '"The numbers ' + d + ' and ' + e + ' and ' + f + ' have a product of ' + y + '."';
 }
 
 function sumArray(ary) {
@@ -26,7 +30,9 @@ function sumArray(ary) {
   for(var i = 0; i < ary.length; i++) {
     result += ary[i];
   }
-  console.log('"' + ary + ' was passed as an array of numbers, and ' + result + ' is their sum."');
+  var prob4 = document.getElementById('prob4');
+  prob4.textContent = '"' + ary + ' was passed as an array of numbers, and ' + result + ' is their sum."';
+  return result;
 }
 
 function multiplyArray(arry) {
@@ -34,5 +40,6 @@ function multiplyArray(arry) {
   for(var i = 0; i < arry.length; i++) {
     total *= arry[i];
   }
-  console.log('"The numbers ' + arry + ' have a product of ' + total + '."');
+  var prob5 = document.getElementById('prob5');
+  prob5.textContent = '"The numbers ' + arry + ' have a product of ' + total + '."';
 }
